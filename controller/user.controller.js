34e82216781884aff1bcfb051a0fb36b2daf1ee5
Abasiodiong Udofia <enalsy22@gmail.com>
2022-11-09 21:49:40 +0100

    user controller

---
+++ controller/user.controller.js
@@ -0,0 +1,24 @@
+const userHandler = () => {
+  //Registers a new user
+  const registerUser = () => {};
+
+  //get a user by userId
+  const getAUserbyId = () => {};
+
+  //list all registered user
+  const listUsers = () => {};
+
+  //modifies a user's profile
+  const updateUserProfileById = () => {};
+
+  //list all registered user
+  const deleteAUserById = () => {};
+
+  return {
+    registerUser,
+    getAUserbyId,
+    listUsers,
+    updateUserProfileById,
+    deleteAUserById,
+  };
+};
